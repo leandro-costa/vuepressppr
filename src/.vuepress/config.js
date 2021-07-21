2b5372f2e7d2655ec086263290755331953fdac1
Leandro Souza <leandro.costa@ifba.edu.br>
2021-07-21 20:47:10 +0000

    first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,102 @@
+const { description } = require('../../package')
+
+module.exports = {
+  base: '/vuepressppr/',
+
+  dest: 'docs',
+
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Programação Orientada a Objetos',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: 'Material para as aulas da disciplina de Programação Orientada a Objetos',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.11/katex.min.css' }],
+    //PWA
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icon-192x192.png' }],
+    ['link', { rel: 'mask-icon', href: '/icon-192x192.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icon-192x192.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    smoothScroll: true,
+    repo: 'leandro-costa/vuepressppr',
+    //repoLabel: '',
+    docsDir: 'src',
+    editLinks: true,
+    editLinkText: 'Edit!',
+    lastUpdated: true,
+
+    sidebarDepth: 2,
+    sidebar: [
+      'home',
+      '01_padroes',
+      '02_SOLID',
+      '03_GOF',
+      '04_nao_GOF'
+    ]
+
+  },
+
+  markdown: {
+
+    extendMarkdown: md => {
+      md.use(require("markdown-it-plantuml"))
+      md.use(require("markdown-it-footnote"))
+      md.use(require("markdown-it-deflist"))
+      md.use(require("markdown-it-katex"))
+    },
+
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    ['@vuepress/plugin-medium-zoom', {
+      options: {
+        margin: 0,
+        background: '#252525'
+      }
+    }],
+    ['container', {
+      type: 'col-wrapper',
+      defaultTitle: '',
+    }],
+    ['container', {
+      type: 'col-half',
+      defaultTitle: '',
+    }],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ]
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: https://portal.ifba.edu.br/santoantonio/imagens-campus-saj/santoantoniodejesus160x240px.jpg
+tagline: Material de Aula
+actionText: Vamos Começar →
+actionLink: home
+footer: Made by Leandro Souza 
+---
+
+![Build badge](https://github.com/leandro-costa/vuepressppr/actions/workflows/node.js.yml/badge.svg)
